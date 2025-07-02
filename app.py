6aa07792445515f87fb08415615fa50e508eeadb
fxhcgg <138549105+fxhcgg@users.noreply.github.com>
2025-07-02 14:00:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def database():
     return render_template('database.html')
 
 if __name__ == '__main__':
-    app.run()
-
+    port = int(os.environ.get("PORT", 5000))  # 读取 Render 提供的端口
+    app.run(host='0.0.0.0', port=port)
